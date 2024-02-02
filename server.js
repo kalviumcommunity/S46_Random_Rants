8e59bf0e6c36279040418cd2c1e8deba0906595f
Anam-Ashraf7 <anam.ashraf.7357@gmail.com>
2024-02-02 14:59:58 +0530

    Connected to Database

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
 const express = require("express")
 const app = express()
-
 const port = 3000
+let connectionStatus = "disconnected"
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.DATABASE_URI)
+        connectionStatus = "Connected"
+    } catch(err) {
+        connectionStatus = "Failed"
+        console.error(err)
+    }
+} 
+
+connectDB()
+
+const disconnectDB = async () => {
+    try {
+        await mongoose.disconnect()
+        connectionStatus = "disconnected"
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+app.get("/", (req,res) => {
+    res.send(connectionStatus)
+})
 
 app.listen(port, () => {
     console.log("Server running...")
 })
 
-app.get("/", (req,res) => {
-    res.send("Hello World!")
-})
 
 app.get("/ping", (req,res) => {
     res.send("pong")
