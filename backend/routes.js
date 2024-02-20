229d05a4141d6ea91a14bf5910b364bbbe119631
Anam-Ashraf7 <anam.ashraf.7357@gmail.com>
2024-02-20 14:17:24 +0530

    bug fixing

--- backend/routes.js
+++ backend/routes.js
@@ -2,10 +2,21 @@ const express = require("express")
 const router = express.Router()
 const {getAllItems,createItem,updateItem,deleteItem} = require("./CRUDController")
 
-router.get("/:model/get", getAllItems)
+// router.get("/:model/get", getAllItems)
 
 router.get("/", (req,res) => {
     res.send("Welcome to Random Rants")
+})
+
+router.get("/user",async (req,res) => {
+    const Model = require("./models/user");
+    try{
+        const items = await Model.find()
+        res.send(items)
+    }catch(err){
+        console.err(err)
+        res.status(500).json({error:"Error fetching items",err})
+    }
 })
 
 router.post("/:model/create",createItem)
